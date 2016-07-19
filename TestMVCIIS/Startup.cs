bdd503d63bb0c76c8d388e5085fea1a693aa8129
jupyt <jupyt@DESKTOP-GF6PA9C>
2016-07-19 17:50:36 +0300

    Add project files.

---
+++ TestMVCIIS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMVCIIS.Startup))]
+namespace TestMVCIIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
