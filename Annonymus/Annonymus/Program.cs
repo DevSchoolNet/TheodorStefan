9f58daa52673e1843e19af1516e027e8859febe7
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2016-07-26 12:11:34 +0300

    Anonymus Method

---
+++ Annonymus/Annonymus/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annonymus
+{
+    delegate void showMe(String a);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            showMe s = delegate (string a)
+              {
+                  Console.WriteLine(a);
+              };
+            s("Aceasta este o metoda anonima!!");
+        }
+    }
+}
