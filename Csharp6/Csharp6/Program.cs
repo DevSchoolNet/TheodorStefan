21f0170a41e257e72ab2a2bf944d2be2893e153a
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2016-07-26 12:17:53 +0300

    Csharp6 functionallity

---
+++ Csharp6/Csharp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Alin";
+            string b = null;
+
+            string c;
+            string d;
+
+            c = a?.Substring(2);
+            d = b?.Substring(2);
+
+            Console.WriteLine(c + " " + d);
+        }
+    }
+}
